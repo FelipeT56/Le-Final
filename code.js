c8ec175431a14eb3cea464b728edb8d5592a1c16
FelipeT56 <144398086+FelipeT56@users.noreply.github.com>
2023-12-10 12:28:29 -0600

    Add files via upload

---
+++ code.js
@@ -0,0 +1,9 @@
+let buttonPress = game.getElementById("button Play Button").innerHTML;
+buttonPress.onclick = funtion();{
+    var die1 = Math.random(1,6);
+    var die2 = Math.random(1,6);
+    if(die1 + die2 == 7 && die1 + die2 == 11){
+        return 
+    }
+};
+
